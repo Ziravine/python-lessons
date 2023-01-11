015a791bc6223f0bd448810237e89a6857f78513
Ziravine <zirangene@gmail.com>
2023-01-10 21:41:12 -0500

    count2 commit

---
+++ count1.py
@@ -0,0 +1,5 @@
+count = 1
+# run loop till count is less than 5
+while count < 5:
+    print(count)
+    count = count + 1
---
+++ count2.py
@@ -0,0 +1,8 @@
+count = 0
+number = 180
+while number > 10:
+    # divide number by 3
+    number = number / 3
+    # increase count
+    count = count + 1
+print('Total iteration required', count)
